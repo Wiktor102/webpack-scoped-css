08d7bf723517152666632435a2386f02f0ec45ed
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-05-28 20:27:51 +0200

    Update example to work with changed getCssLoaders

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const { getCssLoaders } = require('../src/index');
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const { getCssLoaders } = require("../src/index");
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-	mode: isDevelopment ? 'development' : 'production',
-	entry: './src/index.js',
+	mode: isDevelopment ? "development" : "production",
+	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
 	},
 	module: {
 		rules: [
@@ -17,30 +17,30 @@ module.exports = {
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: {
-					loader: require.resolve('babel-loader'),
+					loader: require.resolve("babel-loader"),
 					options: {
-						plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
-					},
-				},
+						plugins: [isDevelopment && require.resolve("react-refresh/babel")].filter(Boolean)
+					}
+				}
 			},
-			getCssLoaders({}, true)
-		],
+			getCssLoaders(isDevelopment, {}, true)
+		]
 	},
 	resolve: {
-		extensions: ['.js', '.jsx'],
+		extensions: [".js", ".jsx"]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: './public/index.html',
+			template: "./public/index.html"
 		}),
 		isDevelopment && new ReactRefreshWebpackPlugin()
 	].filter(Boolean),
 	devServer: {
 		static: {
-			directory: path.join(__dirname, 'dist'),
+			directory: path.join(__dirname, "dist")
 		},
 		port: 9000,
-		hot: true,
+		hot: true
 	},
 	devtool: isDevelopment ? "eval" : false
 };
